1901fc7cb36f5c6cfb086ac1e65f3d1f85eac40d
luelher <luelher@618dbb48-923f-0410-a82f-25073201210d>
2010-05-28 22:35:05 +0000

    Cambio de servidor

--- servicios/config/config.php
+++ servicios/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // symfony directories
-$sf_symfony_lib_dir  = 'C:\AppServ\php5\pear/symfony';
-$sf_symfony_data_dir = 'C:\AppServ\php5\pear\data/symfony';
+$sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/symfony/lib';
+$sf_symfony_data_dir = dirname(__FILE__).'/../lib/symfony/data';
